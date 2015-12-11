f9e3d16b464ccaec969cf0f7e5450459eb3ba6fa
Berlioz <kolesichek@mail.ru>
2015-12-11 12:35:47 +0300

    first commit

---
+++ DictionaryTest/DictionaryTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DictionaryTest
+//
+//  Created by Nikolay Berlioz on 07.09.15.
+//  Copyright (c) 2015 Nikolay Berlioz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DictionaryTest/DictionaryTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DictionaryTest
+//
+//  Created by Nikolay Berlioz on 07.09.15.
+//  Copyright (c) 2015 Nikolay Berlioz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
